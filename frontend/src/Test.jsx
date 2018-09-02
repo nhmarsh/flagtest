044e106a64c7897178ac5b2f1bb712efb60cf3f3
nhmarsh <nhmarsh@gmail.com>
2018-09-01 19:50:59 -0500

    Adds default renderer
    Adds default renderer

--- frontend/src/Test.jsx
+++ frontend/src/Test.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import {FeatureFlag, LaunchDarkly} from 'react-launch-darkly';
+import { FeatureFlag, LaunchDarkly } from 'react-launch-darkly';
+import autoBind from 'react-autobind';
 
 class FlagTest extends React.Component {
   constructor(props) {
     super(props);
+    autoBind(this);
   }
 
-  _renderFeature() {
-    return <span>enabled</span>;
+  renderFeature() {
+    return <span>Enabled!</span>;
+  }
+  renderDefault() {
+    return <span>Disabled!</span>;
   }
 
   render() {
     const user = this.props.match.params.user;
-
+    console.log('Rendering!');
 
     return (<LaunchDarkly clientId="5b8b254f7f3cf0154bde0c66" user={{ key: user }}>
       <div>
           Hello there, {user} - your feature is <FeatureFlag
             flagKey="flagtest"
-            renderFeatureCallback={this._renderFeature}
+            renderFeatureCallback={this.renderFeature}
+            renderDefaultCallback={this.renderDefault}
           />
       </div>
     </LaunchDarkly>);
